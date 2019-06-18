83d511adcb5276f26d7726918ee8d56164d86515
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-18 16:23:05 +0700

    Finished Item 16 Mongoose Connection Helper.

---
+++ 2_users/test/test_helper.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/user_test');
+mongoose.connection
+ .once('open', () => console.log('Good to go!'))
+ .on('error', (error) => {
+     console.warn('Warning', error);
+ });
