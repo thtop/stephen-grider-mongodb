cbf93c1c54da68d8771efa09fd15834ba13bcba3
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-19 11:36:51 +0700

    Finished item 33 Class Based Removvals.

---
+++ 2_users/test/delete_test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleteing a user', () => {
+    let joe;
+
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
+        joe.save()
+            .then(() => done());
+    });
+
+    it('model instance remove', (done) => {
+        joe.remove()
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user === null);
+                done();
+            });
+    });
+
+    it('class method remove', (done) => {
+        // Remove a bunch of records with some given criteria
+        User.remove({ name: 'Joe' })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user === null);
+                done();
+            });
+    });
+
+    it('class method findAndRemove', () => {
+
+    });
+
+    it('class method findByIdAndRemove', () => {
+
+    });
+});
