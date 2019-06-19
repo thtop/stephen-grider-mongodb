a2c5026af6ce058e4f26ec04c118ed17ee35023c
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-19 11:59:51 +0700

    Finished item 37 Model Instance Update.

--- 2_users/test/update_test.js
+++ 2_users/test/update_test.js
@@ -10,7 +10,30 @@ describe('Updating records', () => {
             .then(() => done());
     });
 
-    it('instance type using set n save', () => {
-        
+    it('instance type using set n save', (done) => {
+        joe.set('name', 'Alex');
+        joe.save()
+            .then(() => User.find({}))
+            .then((users) => {
+                assert(users.length === 1);
+                assert(users[0].name === 'Alex');
+                done();
+            });
     });
 });
+
+/**
+ 
+function maybeUpdateName(user) {
+
+}
+
+function maybeUpdateEmail(user) {
+
+}
+
+maybeUpdateName(user);
+maybeUpdateEmail(user);
+user.save();
+
+*/
