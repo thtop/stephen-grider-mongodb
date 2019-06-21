9ed6c43c171886fde338e26f9f5dd5b35d791c99
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-21 17:38:59 +0700

    Finished item 59 Wiring Up Has Many and Has One Relations.

--- 5_schema_design/test/association_test.js
+++ 5_schema_design/test/association_test.js
@@ -4,5 +4,15 @@ const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Assocations', () => {
-    
+    let joe, blogPost, comment;
+
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
+        blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is'});
+        comment = new Comment({ content: 'Congrats on great post' });
+
+        joe.blogPosts.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = joe;
+    });
 });
